7fed5796bcc4fc2251db42babc3f8738be109b84
hichemJedidi <hichem.jedidii@gmail.com>
2023-08-14 03:37:14 +0100

    add Semaphore to concurrency control

--- src/main/java/services/OperationServices.java
+++ src/main/java/services/OperationServices.java
@@ -17,15 +17,24 @@ import util.ObjectMapper;
 
 import java.time.Instant;
 import java.util.Optional;
+import java.util.concurrent.Semaphore;
+
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class OperationServices {
-    private OperationRepository operationRepository;
-    private BankAccountRepository bankAccountRepository;
+    private final OperationRepository operationRepository;
+    private final BankAccountRepository bankAccountRepository;
+
+    private final Semaphore depositSemaphore = new Semaphore(1);
+    private final Semaphore withdrawSemaphore = new Semaphore(1);
+
+
     public AccountDTO deposit(String accountId, long amount) throws NoSuchAccountException{
 
+        try {
+            depositSemaphore.acquire();
         OperationDTO operationDTO = createAndPerformOperation(accountId,amount,OperationType.DEPOSIT);
         if(!bankAccountRepository.findById(accountId).isEmpty()){
             throw new NoSuchAccountException(": "+accountId);
@@ -34,8 +43,17 @@ public class OperationServices {
 
         bankAccount.getOperations().add(ObjectMapper.map(operationDTO,Operation.class));
         return ObjectMapper.map(bankAccount,AccountDTO.class);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Rétablit le statut d'interruption
+            throw new RuntimeException(e);
+        } finally {
+            depositSemaphore.release();
+        }
     }
     public AccountDTO withdraw(String accountId, long amount) throws NoSuchAccountException{
+
+        try {
+            withdrawSemaphore.acquire();
         OperationDTO operationDTO = createAndPerformOperation(accountId,amount,OperationType.WITHDRAW);
         if(!bankAccountRepository.findById(accountId).isEmpty()){
             throw new NoSuchAccountException(": "+accountId);
@@ -43,6 +61,12 @@ public class OperationServices {
         Account bankAccount = bankAccountRepository.findById(accountId).get();
         bankAccount.getOperations().add(ObjectMapper.map(operationDTO,Operation.class));
         return ObjectMapper.map(bankAccount,AccountDTO.class);
+        }  catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Rétablit le statut d'interruption
+            throw new RuntimeException(e);
+        } finally {
+            withdrawSemaphore.release();
+        }
     }
     public OperationDTO createAndPerformOperation(String accountId, long amount, OperationType operationType) throws NoSuchAccountException {
         Optional<Account> optionalBankAccount = bankAccountRepository.findById(accountId);
